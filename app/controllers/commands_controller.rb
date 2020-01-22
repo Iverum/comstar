b2d15687e08cbbe342d15034e7b7d0d5efab10c3
Blake Hair <blake.hair@sigstr.com>
2020-01-21 23:18:51 -0500

    Adding some syntax for dice rolls.

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,6 +1,7 @@
 class CommandsController < ApplicationController
   before_action :validate_command
 
+  DICE_REGEX = /^\d+d\d+$/i
   VALID_CMDS = ["roll"].freeze
 
   def do
@@ -9,13 +10,26 @@ class CommandsController < ApplicationController
 
   private
 
+  def ephemeral(message)
+    return render json: { "response_type": "ephemeral", "text": message }
+  end
+
   def roll
-    render json: { "response_type": "ephemeral", "text": "Hey! You got it!" }
+    return ephemeral "I didn't recognize that command. Try including some dice to roll." unless params[:text]
+
+    # Split up the text
+    possible_dice = params[:text].split
+    # Check that each param meets the structure we expect
+    return ephemeral "All arguments need to be in [number]d[size] format." unless possible_dice.all?{ |arg| DICE_REGEX.match?(arg) }
+
+    # Everything looks okay, so we should create the dice and roll them
+
+    return ephemeral "Hey! You got it!"
   end
 
   def validate_command
     unless VALID_CMDS.include? command
-      render json: { "response_type": "ephemeral", "text": "Sorry, that didn't work. Please try again." }
+      return ephemeral "Sorry, that didn't work. Please try again."
     end
   end
 
--- test/controllers/commands_controller_test.rb
+++ test/controllers/commands_controller_test.rb
@@ -6,9 +6,23 @@ class CommandsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should return error message for no dice" do
+    expected = { "response_type": "ephemeral", "text": "I didn't recognize that command. Try including some dice to roll." }
+    post "/", params: { command: "/roll" }
+    assert_response :success
+    assert_equal expected, response.parsed_body.symbolize_keys
+  end
+
+  test "should return syntax error message for badly formatted dice" do
+    expected = { "response_type": "ephemeral", "text": "All arguments need to be in [number]d[size] format." }
+    post "/", params: { command: "/roll", text: "2d6 nope" }
+    assert_response :success
+    assert_equal expected, response.parsed_body.symbolize_keys
+  end
+
   test "should handle roll" do
     expected = { "response_type": "ephemeral", "text": "Hey! You got it!" }
-    post "/", params: { command: "/roll" }
+    post "/", params: { command: "/roll", text: "2d6 3d10" }
     assert_response :success
     assert_equal expected, response.parsed_body.symbolize_keys
   end
