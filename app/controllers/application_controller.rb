c6475494955408941f1101983c98cf96dec99103
Blake Hair <blake.hair@sigstr.com>
2020-02-02 21:00:27 -0500

    Trying to replace the original message cleanly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::API
   private
 
   def ephemeral(message)
-    return render json: { "response_type": "ephemeral", "text": message, "delete_original": "true" }
+    render json: { "response_type": "ephemeral", "text": message }
   end
 
   def in_channel(message)
-    return render json: { "response_type": "in_channel", "text": message, "delete_original": "true" }
+    render json: { "response_type": "in_channel", "text": message }
   end
 end
--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -31,9 +31,11 @@ class CommandsController < ApplicationController
     return ephemeral "All arguments need to be in [number]d[size] format." unless possible_dice.all?{ |arg| DICE_REGEX.match?(arg) }
 
     # Everything looks okay, so we should create the dice and roll them
+    ephemeral ""
     rolls = Dice.create_dice(possible_dice).map(&:roll)
     total = rolls.inject(0, :+)
-    return in_channel "#{rolls.join("+")}=#{total}"
+    # return in_channel "#{rolls.join("+")}=#{total}"
+    HTTParty.post(params[:response_url], { "response_type": "in_channel", "text": "That worked!" })
   end
 
   def validate_command
