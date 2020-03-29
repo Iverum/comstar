66c19cb949a9d3b349befb08bf4e5b61369e0afb
Blake Hair <blake.hair@gmail.com>
2020-03-29 03:06:08 -0400

    Removing dead code for commands.

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,7 +1,6 @@
 class CommandsController < ApplicationController
   before_action :validate_command
 
-  DICE_REGEX = /^\d+d\d+$/i
   USER_REGEX = /^<@UC\w+\|[\w\.]+>$/
 
   Command.register(:decide)
@@ -17,26 +16,6 @@ class CommandsController < ApplicationController
   end
 
   private
-
-  def roll
-    return ephemeral "I didn't recognize that command. Try including some dice to roll." unless params[:text].present?
-
-    # Split up the text
-    possible_dice = params[:text].split
-    # Check that each param meets the structure we expect
-    return ephemeral "All arguments need to be in [number]d[size] format." unless possible_dice.all?{ |arg| DICE_REGEX.match?(arg) }
-
-    dice = Dice.create_dice(possible_dice)
-    return ephemeral "You can only roll up to 20 dice at a time." unless dice.length <= 20
-
-    return ephemeral "You can only roll up to a d100." if dice.any? { |d| d.size > 100 }
-
-    acknowledge
-    # Everything looks okay, so we should roll the dice
-    rolls = dice.map(&:roll)
-    total = rolls.inject(0, :+)
-    delay_in_channel("<@#{params[:user_id]}> rolled #{params[:text]}:\n #{rolls.join("+")}=#{total}")
-  end
 
   def validate_command
     unless Command.valid? command
